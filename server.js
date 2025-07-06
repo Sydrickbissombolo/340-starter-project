5206b819a0dc346e880f553c33292a82f481c5e3
Sydrickbissombolo <newtonbissombolo2@gmail.com>
2025-07-06 14:59:33 -0600

    HTML validation errors cleaned up!

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-/* ******************************************
+/*******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+
+/* ***********************
+ * Middleware to serve static files
+ *************************/
+app.use(express.static("public"));
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app. use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // Index Route
-app.get("/", function(req, res){
-  res.render("index", { title: "Home" })
-})
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT 
 const host = process.env.HOST
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
