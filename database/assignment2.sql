13294e581692cc2e10a66f4f34d3bcff83e815dc
Sydrickbissombolo <newtonbissombolo2@gmail.com>
2025-07-12 02:30:10 -0600

    databse completion

---
+++ database/assignment2.sql
@@ -0,0 +1,36 @@
+INSERT INTO account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+) VALUES (
+    'Tony',
+    'Stark',
+    'tony@starkent.com',
+    'Iam1ronM@n'
+);
+
+-- Update Tony Stark's account type to 'Admin'
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- Delete Tony Stark's account
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+-- Fix GM Description
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- Join to Select Inventory with 'Sport' classification
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+-- Update file paths for images and thumbnails
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, 'images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, 'images/', '/images/vehicles/');
